8f6499da349fd9a0c8c50e6fdc93e4532c224609
Luis Medina <luisinmedina@gmail.com>
2021-12-01 03:58:12 -0400

    IMAGE COMP Changes

--- service.py
+++ service.py
@@ -1,5 +1,13 @@
 # Developed by Mirko J. Rodríguez mirko.rodriguezm@gmail.com
 #Reference: https://towardsdatascience.com/deploying-keras-models-using-tensorflow-serving-and-flask-508ba00f1037
+import numpy as np
+import pandas as pd
+import cv2 as cv 
+from google.colab.patches import cv2_imshow # for image display
+from skimage import io
+from PIL import Image 
+import matplotlib.pylab as plt
+
 
 #Import Flask
 from flask import Flask, request, jsonify, redirect
@@ -39,6 +47,34 @@ def allowed_file(filename):
 def main_page():
 	return '¡Servicio REST activo!'
 
+@app.route('/')
+@cross_origin()
+def main_page():
+	# Create a list to store the urls of the images
+    urls = ["https://iiif.lib.ncsu.edu/iiif/0052574/full/800,/0/default.jpg",
+        "https://iiif.lib.ncsu.edu/iiif/0016007/full/800,/0/default.jpg",
+        "https://placekitten.com/800/571"]  
+    # Read and display the image
+    # loop over the image URLs, you could store several image urls in the list
+
+    for url in urls:
+        image = io.imread(url) 
+        image_2 = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        final_frame = cv.hconcat((image, image_2))
+        cv2_imshow(final_frame)
+        print('\n')
+    #--- take the absolute difference of the images ---
+    res = cv2.absdiff(image, image_2)
+
+    #--- convert the result to integer type ---
+    res = res.astype(np.uint8)
+
+    #--- find percentage difference based on number of pixels that are not zero ---
+    percentage = (np.count_nonzero(res) * 100)/ res.size
+
+    return percentage
+
+
 @app.route('/model/covid19/', methods=['GET','POST'])
 @cross_origin()
 def default():
@@ -71,7 +107,7 @@ def default():
             	
 		# Resultados
             	prediction = 1 if (result >= 0.5) else 0
-            	CLASSES = ['Normal', 'Covid19+']
+            	CLASSES = ['Normal', 'Con Cancer']
 
             	ClassPred = CLASSES[prediction]
             	ClassProb = result
